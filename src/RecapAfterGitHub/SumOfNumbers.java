65e6664916b72839ee347f014117bcb5efd9ea46
Wualter01 <kidink1993@gmail.com>
2022-10-28 17:22:46 -0400

    calculator task class

---
+++ src/RecapAfterGitHub/SumOfNumbers.java
@@ -0,0 +1,15 @@
+package RecapAfterGitHub;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        /*TASK: write a do-while loop that asks the user to enter two numbers.
+        the numbers should be added and the sum displayed.
+        the loop should ask the user whether he or she wishes to perform the operation again.
+        if so, the loop should repeat: otherwise it should terminate.
+        if user says y or Y continue
+        if user says n or N terminate the loop
+         */
+
+
+    }
+}
